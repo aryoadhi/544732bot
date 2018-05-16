a1d7c05b0afcaf2df684858b330cc6a9cf5d408a
Ryu Sei <aryoadhi@users.noreply.github.com>
2018-05-16 15:14:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,4 +14,4 @@ async def on_ready():
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
 
-bot.run('NDQ2MTQ1Njg1MTkzMDk3MjI2.Dd1zzg.nFyskkj31NdFYKCk-pm3HMrWad8')
+bot.run('<CODE>')
